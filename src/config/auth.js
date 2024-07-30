1058ed39dc2829ed6a64b60f80734d2f2439c543
Gustavo Areco <gujoar82@gmail.com>
2024-07-30 14:19:02 -0300

    auth

--- src/config/auth.js
+++ src/config/auth.js
@@ -2,7 +2,7 @@ import passport from "passport"
 import { Strategy as LocalStrategy } from "passport-local" 
 import GitHubStrategy from "passport-github2" 
 import jwt from "jsonwebtoken" 
-import User from "../dao/Models/user.model.js"
+import User from "../dao/models/user.model.js"
 import bcrypt from "bcrypt" 
 import { entorno } from "./config.js"
 
